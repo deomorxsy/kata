3e140037e1a52ebd322faa5afb16d6666d1f8499
Asari Schneider <32879431+deomorxsy@users.noreply.github.com>
2019-06-04 14:06:50 -0300

    adding the exercises from the book PensePython

---
+++ PensePython2e/10-1-Func_nested_sum.py
@@ -0,0 +1,14 @@
+#! usr/bin/env/python3
+
+def nested_sum():
+	#code
+	return sum(i) for i in t
+
+
+t = [[1, 2], [3], [4, 5, 6]]
+
+print(t) #debugger
+print(nested_sum(t)) #debugger
+
+if '__name__' == '__main__':
+	main()
---
+++ PensePython2e/10-1-nested_sum.py
@@ -0,0 +1,16 @@
+#! usr/bin/env/python3
+
+def nested_sum():
+	cont = 0
+
+	for i in t:
+		return cont += sum(i)
+
+t = [[1, 2], [3], [4, 5, 6]]
+
+print(t) #debugger
+print(nested_sum(t)) #debugger
+
+
+if '__name__' == '__main__':
+	main()
