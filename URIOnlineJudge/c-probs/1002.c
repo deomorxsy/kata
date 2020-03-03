c90212054f3565aed299badd2609d7f18a8b7592
Asari Schneider <32879431+deomorxsy@users.noreply.github.com>
2020-03-03 19:35:38 -0300

    committing a c problem (and a new directory) from URIOnlineJudge.

---
+++ URIOnlineJudge/c-probs/1002.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	float pi = 3.14159;
+	float raio;
+	float area;
+	// define the variables with its types.
+
+	scanf("%f", &raio); // reads the input as float and assigns the value to the variable
+	area = pi * (raio * 2); // calculates the area
+
+	printf("A=%4f", area); // prints the area, type float, with 4 decimal points.
+
+}
