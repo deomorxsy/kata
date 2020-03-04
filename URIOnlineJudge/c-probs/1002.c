5f459329c8bcf5cc92319fd037f444568fa697e9
Asari Schneider <32879431+deomorxsy@users.noreply.github.com>
2020-03-03 22:38:03 -0300

    fixing errors

--- URIOnlineJudge/c-probs/1002.c
+++ URIOnlineJudge/c-probs/1002.c
@@ -2,14 +2,20 @@
 // date: 2020-03-03
 
 int main() {
-	float pi = 3.14159;
-	float raio;
-	float area;
+	double pi = 3.14159;
+	double raio;
+	double area;
 	// define the variables with its types.
 
-	scanf("%f", &raio); // reads the input as float and assigns the value to the variable
-	area = pi * (raio * 2); // calculates the area
+	scanf("%lf", &raio); // reads the input as double (long float) and assigns the value to the variable
+	area = pi * (raio * raio); // calculates the area
 
-	printf("A=%4f", area); // prints the area, type float, with 4 decimal points.
+	printf("A=%.4lf\n", area); // prints the area, type double (long float), with 4 decimal points.
 
+	return 0;
 }
+
+/*
+to round: "%2.f";
+
+*/
