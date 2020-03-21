614eb93d30def12b0dd215435543f4cdb5f9390c
Asari Schneider <32879431+deomorxsy@users.noreply.github.com>
2020-03-20 23:59:25 -0300

    solving problems

---
+++ URIOnlineJudge/c-probs/1001.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, x;
+
+	scanf("\n%d", &a);
+	scanf("\n%d", &b);
+	
+	x = a + b;
+	printf("X = %d\n", x);
+
+}
---
+++ URIOnlineJudge/c-probs/1003.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, SOMA;
+
+    scanf("\n%d \n%d", &a, &b);
+
+    SOMA = a + b;
+    printf("SOMA = %d\n", SOMA);
+}
