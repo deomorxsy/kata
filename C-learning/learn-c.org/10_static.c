a7c5f38d50b82094e501121be0a8e053f82d8737
PAIXÃO, L <32879431+deomorxsy@users.noreply.github.com>
2020-05-22 18:06:52 -0300

    chapter 10

---
+++ C-learning/learn-c.org/10_static.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int sum (int num) {
+/**
+* find sum to n numbers	
+*/
+	static int var = 0;
+	var += num;
+	return var;
+}
+
+int main() {
+	printf("%d ",sum(55));
+	printf("%d ",sum(45));
+	printf("%d ",sum(50));
+ 	
+ 	return 0;
+}
