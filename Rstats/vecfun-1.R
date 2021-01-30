4eecb41b64a4164cdb60ada0a026124f9d7ce8e3
asari <32879431+deomorxsy@users.noreply.github.com>
2021-01-30 01:22:47 -0300

    starting list of [r] exercises

---
+++ Rstats/vecfun-1.R
@@ -0,0 +1,20 @@
+#!/usr/bin/R
+#30/01/2021
+
+#setwd("~/Documentos/coding/projetos/rscripts/bib-packrat")
+#packrat::on("~/Documentos/coding/projetos/rscripts/bib-packrat")
+
+# Exercise 1: Vectors and Functions
+#https://www.r-exercises.com/2019/08/12/vectors-and-functions
+
+#1.1
+rivstats <- c(length(rivers), sum(rivers), mean(rivers), median(rivers), var(rivers),
+                sd(rivers), min(rivers), max(rivers))
+rivstats
+
+#1.2
+#Média truncada: valor da porcentagem * número total de amostras.
+#O valor do produto é retirado dos extremos da amostra.
+#abaixo: 0.25 * 8 = 2.0 || mean(1, 2, 3, 6) = 3.
+trunmed <- mean(c(-100, 0, 1, 2, 3, 6, 50, 73), trim=0.25)
+trunmed
